0a9cc47761f8f774597d6d16fad23a771ece57b7
Timur Bazhirov <timur@exabyte.io>
2024-12-12 18:30:01 -0800

    chore: add example validator test

--- src/py/camsai/standards/__init__.py
+++ src/py/camsai/standards/__init__.py
@@ -1,4 +1,22 @@
-import numpy as np
+from pydantic import BaseModel, ValidationError
 
-def get_length(vec: np.ndarray) -> np.float_:
-    return np.linalg.norm(vec)
+
+def is_valid_with_pydantic(example: dict, pydantic_model: BaseModel):
+    """
+    Validates a given example against the base model.
+
+    Args:
+        example (dict): example to validate.
+        pydantic_model (BaseModel): base model to validate the example with.
+
+    Returns:
+        bool: True if the example is valid, False otherwise.
+    """
+    try:
+        pydantic_model(**example)
+        return True
+    except ValidationError as e:
+        return False
+
+
+is_valid = is_valid_with_pydantic
