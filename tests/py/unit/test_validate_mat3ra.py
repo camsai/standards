2a7cf79f8eed359b6c5e06f31c4e47fe338c7e9c
Timur Bazhirov <timur@exabyte.io>
2024-12-12 20:14:12 -0800

    chore: update test name

--- tests/py/unit/test_validate_mat3ra.py
+++ tests/py/unit/test_validate_mat3ra.py
@@ -3,7 +3,7 @@ from camsai.standards.mat3ra import MaterialSchema as Mat3raMaterialSchema
 from mat3ra.esse.data.examples import EXAMPLES
 
 
-def test_validate_pydantic_matera():
+def test_validate_mat3ra_material():
     example_material = next((e for e in EXAMPLES if e["path"] == "material"), None)["data"]
     # TODO: figure out why derivedProperties is not in the schema
     example_material["derivedProperties"] = []
